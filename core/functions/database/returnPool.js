7dac87277494a2d9032f8f0598882482b29841f0
Ciborn <rob1.bourachot@gmail.com>
2018-03-17 00:40:40 +0100

    DB Security Fix

--- core/functions/database/returnPool.js
+++ core/functions/database/returnPool.js
@@ -5,7 +5,6 @@ var pool = mysql.createPool({
     host: config.mysql.host,
     user: config.mysql.user,
     password: config.mysql.password,
-    database: config.mysql.database,
-    insecureAuth: true
+    database: config.mysql.database
 });
 module.exports = pool;
