86325f6b30b10e429931a1f6e16005a4532146c8
Ciborn <rob1.bourachot@gmail.com>
2018-03-17 11:24:46 +0100

    Profile Bug Fixes

--- core/commands/profile.js
+++ core/commands/profile.js
@@ -1,9 +1,10 @@
 const isEmpty = require('./../functions/utils/isEmpty');
 const showProfile = require('./../functions/activity/showProfile');
+const Discord = require('discord.js');
 module.exports = function(bot, message, args) {
-    const sendProfile = function(userId) {
+    const sendProfile = function(bot, userId) {
         showProfile(bot, userId).then(embed => {
-            embed = embed.setColor(message.member.displayColor);
+            embed = embed.setColor(bot.guilds.find('id', '379115766127001600').members.find('id', userId).highestRole.displayColor);
             message.channel.send(embed);
         }).catch(err => {
             const embed = new Discord.RichEmbed()
