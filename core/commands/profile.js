ddcbef012523ccfbe427e5e2613c64f2905e78cc
Ciborn <rob1.bourachot@gmail.com>
2018-03-17 11:19:50 +0100

    Others User's Profile Update

--- core/commands/profile.js
+++ core/commands/profile.js
@@ -1,12 +1,8 @@
 const isEmpty = require('./../functions/utils/isEmpty');
 const showProfile = require('./../functions/activity/showProfile');
 module.exports = function(bot, message, args) {
-    if (message.mentions.members.size > 0) {
-
-    } else if (!isEmpty(args)) {
-
-    } else {
-        showProfile(bot, message.author.id).then(embed => {
+    const sendProfile = function(userId) {
+        showProfile(bot, userId).then(embed => {
             embed = embed.setColor(message.member.displayColor);
             message.channel.send(embed);
         }).catch(err => {
@@ -17,5 +13,17 @@ module.exports = function(bot, message, args) {
                 .setColor('RED');
             message.channel.send({embed});
         })
+    }
+
+    if (message.mentions.members.size > 0) {
+        sendProfile(bot, message.mentions.members.first().user.id);
+    } else if (!isEmpty(args)) {
+        bot.fetchUser(args[0]).then(user => {
+            sendProfile(bot, user.id);
+        }).catch(err => {
+            message.channel.send(`Sorry **${message.author.username}**, but the user ID you gave doesn't match with a valid user.`);
+        })
+    } else {
+        sendProfile(bot, message.author.id);
     }
 }
