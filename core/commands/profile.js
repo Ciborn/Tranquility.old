b8e4d093ecde5a90bacfdd71ada3bc3b7b58e8be
Ciborn <rob1.bourachot@gmail.com>
2018-03-17 11:29:25 +0100

    Profile Color Bug Fix

--- core/commands/profile.js
+++ core/commands/profile.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports = function(bot, message, args) {
     const sendProfile = function(bot, userId) {
         showProfile(bot, userId).then(embed => {
-            embed = embed.setColor(bot.guilds.find('id', '379115766127001600').members.find('id', userId).highestRole.displayColor);
+            embed = embed.setColor(bot.guilds.find('id', '379115766127001600').members.find('id', userId).highestRole.color);
             message.channel.send(embed);
         }).catch(err => {
             const embed = new Discord.RichEmbed()
